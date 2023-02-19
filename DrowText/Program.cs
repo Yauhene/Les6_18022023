8e0ed1f07ed17c6c684b61d594311d56b480f3ef
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-02-19 08:07:08 +0300

    DrowText gone

--- DrowText/Program.cs
+++ DrowText/Program.cs
@@ -6,4 +6,12 @@ void DrowText(string text, int left, int top)
     Console.WriteLine(text);
 }
 
-DrowText("Fucking World...", 30,30);
+Console.Clear();
+for(int i=0; i<=50; i=i+10)
+{
+    DrowText("Fucking World...", i, 30);
+    //DrowText("*", i, 30);
+    Console.WriteLine($"Отступ сверху {i} позиций");
+    Console.ReadLine();
+
+}
