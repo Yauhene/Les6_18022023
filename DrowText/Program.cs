e937aa753adf1527f925a02fd5c755fd09ed846f
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-02-19 07:52:13 +0300

    DrowText typed

---
+++ DrowText/Program.cs
@@ -0,0 +1,9 @@
+﻿// Нечто выводящее текст. Якобы.
+
+void DrowText(string text, int left, int top)
+{
+    Console.SetCursorPosition(left, top);
+    Console.WriteLine(text);
+}
+
+DrowText("Fucking World...", 30,30);
